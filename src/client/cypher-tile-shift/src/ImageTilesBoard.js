b44a70efcd9861575d666f295ed6980ab857c749
TinkerTavern <lancaster.t.com@gmail.com>
2022-09-15 22:49:29 +0100

    - Added method to create new tiles from images (both 3x3 and 4x4) - Added option to switch between 3x3 and 4x4 mode (requires rebuild currently)

--- src/client/cypher-tile-shift/src/ImageTilesBoard.js
+++ src/client/cypher-tile-shift/src/ImageTilesBoard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from 'rebass'
+import {Box} from 'rebass'
 import 'animate.css'
 import './App.css'
 import ladder from './images/ladder.png'
@@ -11,233 +11,276 @@ import office from './images/office.jpg'
 
 /**
  * Handy way of importing all files in a folder together
- * 
+ *
  * From here: https://stackoverflow.com/questions/53777194/webpack-dynamic-import-method-returns-empty-array-instead-of-files-in-reactjs-h
  */
 function importAll(r) {
-	return r.keys().map(r);
+    return r.keys().map(r);
 }
-const caveImages = importAll(require.context('./images/cave4x4', false, /\.(png|jpe?g|svg)$/));
-const planetImages = importAll(require.context('./images/planet4x4', false, /\.(png|jpe?g|svg)$/));
-const rocketImages = importAll(require.context('./images/rocket4x4', false, /\.(png|jpe?g|svg)$/));
-const spaceImages = importAll(require.context('./images/space4x4', false, /\.(png|jpe?g|svg)$/));
-const officeImages = importAll(require.context('./images/office4x4', false, /\.(png|jpe?g|svg)$/));
+
+const mode = 3;
+var caveImages, planetImages, rocketImages, spaceImages, officeImages
+
+if (mode === 4) {
+    caveImages = importAll(require.context('./images/cave4x4', false, /\.(png|jpe?g|svg)$/));
+    planetImages = importAll(require.context('./images/planet4x4', false, /\.(png|jpe?g|svg)$/));
+    rocketImages = importAll(require.context('./images/rocket4x4', false, /\.(png|jpe?g|svg)$/));
+    spaceImages = importAll(require.context('./images/space4x4', false, /\.(png|jpe?g|svg)$/));
+    officeImages = importAll(require.context('./images/office4x4', false, /\.(png|jpe?g|svg)$/));
+} else {
+    caveImages = importAll(require.context('./images/cave3x3', false, /\.(png|jpe?g|svg)$/));
+    planetImages = importAll(require.context('./images/planet3x3', false, /\.(png|jpe?g|svg)$/));
+    rocketImages = importAll(require.context('./images/rocket3x3', false, /\.(png|jpe?g|svg)$/));
+    spaceImages = importAll(require.context('./images/space3x3', false, /\.(png|jpe?g|svg)$/));
+    officeImages = importAll(require.context('./images/office3x3', false, /\.(png|jpe?g|svg)$/));
+}
 
 /**
  * Basic animate css template from https://animate.style/
  */
 const animateCSS = (element, animation, prefix = 'animate__') =>
-	// We create a Promise and return it
-	new Promise((resolve, reject) => {
-		const animationName = `${prefix}${animation}`;
-		const node = document.querySelector(element);
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.querySelector(element);
 
-		node.classList.add(`${prefix}animated`, animationName);
+        node.classList.add(`${prefix}animated`, animationName);
 
-		// When the animation ends, we clean the classes and resolve the Promise
-		function handleAnimationEnd(event) {
-			event.stopPropagation();
-			node.classList.remove(`${prefix}animated`, animationName);
-			resolve('Animation ended');
-		}
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
 
-		node.addEventListener('animationend', handleAnimationEnd, {once: true});
-	});
+        node.addEventListener('animationend', handleAnimationEnd, {once: true});
+    });
 
 class Tile extends React.Component {
-	render () {
-		return (
-			<button
-				className='tile'
-				onClick={ () => {this.props.onClick ()} }
-			>
-			<img draggable={false} src={this.props.tileImage} alt={this.props.alt} />
-			</button>
-		);
-	}
+    render() {
+        return (
+            <button
+                className='tile'
+                onClick={() => {
+                    this.props.onClick()
+                }}
+            >
+                <img draggable={false} src={this.props.tileImage} alt={this.props.alt}/>
+            </button>
+        );
+    }
 }
 
 class Board extends React.Component {
-	constructor (props) {
-		super (props);
+    constructor(props) {
+        super(props);
 
-		const imageIdx = Math.floor(Math.random() * 5);
-		var imageSet;
-		var imagePreview;
-		switch (imageIdx) {
-			case 0:
-				imageSet = caveImages;
-				imagePreview = cave;
-				break;
-			case 1:
-				imageSet = planetImages;
-				imagePreview = planet;
-				break;
-			case 2:
-				imageSet = rocketImages;
-				imagePreview = rocket;
-				break;
-			case 3:
-				imageSet = officeImages;
-				imagePreview = office;
-				break;
-			default:
-				imageSet = spaceImages;
-				imagePreview = space;
-		}
+        const imageIdx = Math.floor(Math.random() * 5);
+        var imageSet;
+        var imagePreview;
+        switch (imageIdx) {
+            case 0:
+                imageSet = caveImages;
+                imagePreview = cave;
+                break;
+            case 1:
+                imageSet = planetImages;
+                imagePreview = planet;
+                break;
+            case 2:
+                imageSet = rocketImages;
+                imagePreview = rocket;
+                break;
+            case 3:
+                imageSet = officeImages;
+                imagePreview = office;
+                break;
+            default:
+                imageSet = spaceImages;
+                imagePreview = space;
+        }
 
-		this.state = {
-			active: false,
-			tiles: [...Array (16).keys ()],
-			images: imageSet,
-			preview: imagePreview,
-			currentSelection: -1,
-			currentTimeRemaining: this.props.timeToComplete,
-			timeString: '',
-		};
-	}
+        if (mode === 4) {
 
-	startPuzzleTimer () {
-		this.timerID = setInterval (() =>
-			this.tick (), 
-			1000
-		);
-	}
+            this.state = {
+                active: false,
+                tiles: [...Array(16).keys()],
+                images: imageSet,
+                preview: imagePreview,
+                currentSelection: -1,
+                currentTimeRemaining: this.props.timeToComplete,
+                timeString: '',
+            };
+        } else {
+            this.state = {
+                active: false,
+                tiles: [...Array(9).keys()],
+                images: imageSet,
+                preview: imagePreview,
+                currentSelection: -1,
+                currentTimeRemaining: this.props.timeToComplete,
+                timeString: '',
+            };
+        }
+    }
 
-	showImagePreview () {
-		// Show the image preview for a set number of seconds
-		setTimeout (() => {
-			animateCSS('.image-preview', 'fadeOut').then((message) => {
-				this.setState ({active: true})
-				this.startPuzzleTimer ()
-			});
-		}, this.props.imagePreviewTime * 1000);
-	}
+    startPuzzleTimer() {
+        this.timerID = setInterval(() =>
+                this.tick(),
+            1000
+        );
+    }
 
-	componentDidMount () {
-		// Randomize the tile list
-		let tilesArr = this.state.tiles;
-		this.shuffleArray (tilesArr)
-		this.setState ({tiles: tilesArr})
+    showImagePreview() {
+        // Show the image preview for a set number of seconds
+        setTimeout(() => {
+            animateCSS('.image-preview', 'fadeOut').then((message) => {
+                this.setState({active: true})
+                this.startPuzzleTimer()
+            });
+        }, this.props.imagePreviewTime * 1000);
+    }
 
-		if (this.props.timeToComplete < 10) {
-			this.setState ({timeString: '0' + this.props.timeToComplete});
-		} else {
-			this.setState ({timeString: '' + this.props.timeToComplete})
-		}
+    componentDidMount() {
+        // Randomize the tile list
+        let tilesArr = this.state.tiles;
+        this.shuffleArray(tilesArr)
+        this.setState({tiles: tilesArr})
 
-		if (this.props.showImagePreview) {
-			this.showImagePreview ()
-		} else {
-			this.setState ({active: true})
-			this.startPuzzleTimer ()
-		}
-	}
+        if (this.props.timeToComplete < 10) {
+            this.setState({timeString: '0' + this.props.timeToComplete});
+        } else {
+            this.setState({timeString: '' + this.props.timeToComplete})
+        }
 
-	componentWillUnmount () {
-		clearInterval (this.timerID);
-	}
+        if (this.props.showImagePreview) {
+            this.showImagePreview()
+        } else {
+            this.setState({active: true})
+            this.startPuzzleTimer()
+        }
+    }
 
-	/* Randomize array in-place using Durstenfeld shuffle algorithm */
-	shuffleArray(array) {
-		for (var i = array.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-	}
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
-	tick () {
-		let timeRemaining = this.state.currentTimeRemaining - 1;
-		if (timeRemaining < 0) {
-			this.props.onFailure ()
-		} else {
-			if (timeRemaining < 10) {
-				this.setState ({timeString: '0' + timeRemaining});
-			} else {
-				this.setState ({timeString: '' + timeRemaining})
-			}
-			this.setState ({currentTimeRemaining: timeRemaining})
-		}
-	}
+    /* Randomize array in-place using Durstenfeld shuffle algorithm */
+    shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
-	handleClick (i) {
-		if (this.state.currentSelection === -1) {
-			this.setState ({currentSelection: i})
-		} else {
-			let tit = i
-			let tat = this.state.currentSelection
-			let newTiles = this.state.tiles;
+    tick() {
+        let timeRemaining = this.state.currentTimeRemaining - 1;
+        if (timeRemaining < 0) {
+            this.props.onFailure()
+        } else {
+            if (timeRemaining < 10) {
+                this.setState({timeString: '0' + timeRemaining});
+            } else {
+                this.setState({timeString: '' + timeRemaining})
+            }
+            this.setState({currentTimeRemaining: timeRemaining})
+        }
+    }
 
-			let temp = newTiles[tit]
-			newTiles[tit] = newTiles[tat]
-			newTiles[tat] = temp
+    handleClick(i) {
+        if (this.state.currentSelection === -1) {
+            this.setState({currentSelection: i})
+        } else {
+            let tit = i
+            let tat = this.state.currentSelection
+            let newTiles = this.state.tiles;
 
-			this.setState ({tiles: newTiles, currentSelection: -1})
-			this.checkSolution (); // Check if we're correct
-		}
-	}
+            let temp = newTiles[tit]
+            newTiles[tit] = newTiles[tat]
+            newTiles[tat] = temp
 
-	checkSolution () {
-		var isAscending = a => a.slice(1).every(this.getCondition(a));
-		if (isAscending (this.state.tiles)) {
-			this.props.onSolution ()
-		}
-	}
+            this.setState({tiles: newTiles, currentSelection: -1})
+            this.checkSolution(); // Check if we're correct
+        }
+    }
 
-	getCondition(a) {
-		console.log(a)
-		return (e, i) => e > a[i];
-	}
+    checkSolution() {
+        var isAscending = a => a.slice(1).every(this.getCondition(a));
+        if (isAscending(this.state.tiles)) {
+            this.props.onSolution()
+        }
+    }
 
-	renderTile (i) {
-		return (
-			<Tile
-				tileImage={this.state.images[this.state.tiles[i]]}
-				onClick={ () => this.handleClick (i) }
-				alt={"Tile " + i}
-			/>
-		)
-	}
+    getCondition(a) {
+        console.log(a)
+        return (e, i) => e > a[i];
+    }
 
-	render () {
+    renderTile(i) {
+        return (
+            <Tile
+                tileImage={this.state.images[this.state.tiles[i]]}
+                onClick={() => this.handleClick(i)}
+                alt={"Tile " + i}
+            />
+        )
+    }
 
-		var mainView;
-		if (this.state.active) {
-			mainView = <Box
-				sx={{
-				display: 'grid',
-				gridGap: 1,
-				gridTemplateColumns: [
-					'repeat(4, 1fr)',
-					'repeat(4, 1fr)'
-				],
-				}}>
-				{[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].map ((tileIdx) => <Box key={tileIdx}>{this.renderTile (tileIdx)}</Box>)}
-			</Box>
-		} else {
-			mainView = <Box>
-				<img draggable={false} className="image-preview" src={this.state.preview} alt="alt text" />
-			</Box>
-		}
+    render() {
 
-		return (
-			<div>
-				<img draggable={false} className="background-image" src={ladder} alt="Echo Games' ladder"/>
-				<Box
-					sx={{
-					maxWidth: 768,
-					mx: 'auto',
-					px: 3,
-					py: 4
-				}}>
-				<div className="timer">Time left: {this.state.timeString}</div>
-				</Box>
-				{mainView}
-			</div>
-		)
-	}
+        var mainView;
+        if (this.state.active) {
+            if (mode === 4) {
+                mainView = <Box
+                    sx={{
+                        display: 'grid',
+                        gridGap: 1,
+                        gridTemplateColumns: [
+                            'repeat(4, 1fr)',
+                            'repeat(4, 1fr)'
+                        ],
+                    }}>
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((tileIdx) => <Box
+                        key={tileIdx}>{this.renderTile(tileIdx)}</Box>)}
+                </Box>
+            } else {
+                mainView = <Box
+                    sx={{
+                        display: 'grid',
+                        gridGap: 1,
+                        gridTemplateColumns: [
+                            'repeat(3, 1fr)',
+                            'repeat(3, 1fr)'
+                        ],
+                    }}>
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((tileIdx) => <Box
+                        key={tileIdx}>{this.renderTile(tileIdx)}</Box>)}
+                </Box>
+            }
+        } else {
+            mainView = <Box>
+                <img draggable={false} className="image-preview" src={this.state.preview} alt="alt text"/>
+            </Box>
+        }
+
+        return (
+            <div>
+                <img draggable={false} className="background-image" src={ladder} alt="Echo Games' ladder"/>
+                <Box
+                    sx={{
+                        maxWidth: 768,
+                        mx: 'auto',
+                        px: 3,
+                        py: 4
+                    }}>
+                    <div className="timer">Time left: {this.state.timeString}</div>
+                </Box>
+                {mainView}
+            </div>
+        )
+    }
 }
 
-export { Tile, Board }
+export {Tile, Board}
--- src/client/cypher-tile-shift/src/images/imageChopper.py
+++ src/client/cypher-tile-shift/src/images/imageChopper.py
@@ -1,14 +1,39 @@
+import os
+
 from PIL import Image
-from itertools import product
 
 
-def tile(filename, dir_in, dir_out, d):
-    name, ext = os.path.splitext(filename)
-    img = Image.open(os.path.join(dir_in, filename))
-    w, h = img.size
+def createSplits(image_path):
+    split(image_path, 4, 4, False)
+    split(image_path, 3, 3, False)
 
-    grid = product(range(0, h - h % d, d), range(0, w - w % d, d))
-    for i, j in grid:
-        box = (j, i, j + d, i + d)
-        out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
-        img.crop(box).save(out)
+
+def split(image_path, rows, cols, should_cleanup):
+    im = Image.open(image_path)
+    im_width, im_height = im.size
+    row_width = int(im_width / rows)
+    row_height = int(im_height / cols)
+    n = 0
+    name, ext = os.path.splitext(image_path)
+    newDir = name + str(rows) + "x" + str(cols) + "/"
+    os.mkdir(newDir)
+    for i in range(0, rows):
+        for j in range(0, cols):
+            box = (j * row_width, i * row_height, j * row_width +
+                   row_width, i * row_height + row_height)
+            outp = im.crop(box)
+            outp_path = newDir + name + "_" + str(chr(ord('a') + n)) + ext
+            print("Exporting image tile: " + outp_path)
+            outp.save(outp_path)
+            n += 1
+    if should_cleanup:
+        print("Cleaning up: " + image_path)
+        os.remove(image_path)
+
+
+if __name__ == '__main__':
+    createSplits("cave.jpg")
+    createSplits("rocket.jpg")
+    createSplits("planet.jpg")
+    createSplits("office.jpg")
+    createSplits("space.jpg")
