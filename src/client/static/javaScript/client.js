b5d314965c45e4ea524937a50c62cf824d997ebc
TinkerTavern <lancaster.t.com@gmail.com>
2022-09-21 10:41:35 +0100

    - Added ability to change the server URL with test to ensure it's correct

--- src/client/static/javaScript/client.js
+++ src/client/static/javaScript/client.js
@@ -3,7 +3,24 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 function setAddr() {
-    localStorage.setItem("addr", document.getElementById("addr").value)
+    let url = document.getElementById("addr").value !== "" ? document.getElementById("addr").value : "127.0.0.1"
+    url = "http://" + url + ":3000"
+    $.ajax({
+        url: url + "/ping",
+        dataType: "json",
+        success: function (data) {
+            console.log(data['ping'])
+            if (data['ping'] === "pong") {
+                localStorage.setItem("addr", document.getElementById("addr").value)
+                alert("Server address changed successfully")
+            } else {
+                alert("Server address not changed, server config issue")
+            }
+        },
+        error: function () {
+            alert("Server address not changed, server not found")
+        }
+    });
 }
 
 function clearConfigs() {
